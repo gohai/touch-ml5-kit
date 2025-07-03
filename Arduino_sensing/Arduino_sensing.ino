15cbf90b8c1de79ee9bc015bd468fbb4f8ba78f7
gohai <gottfried.haider@gmail.com>
2025-07-03 18:26:51 +0800

    Clean up Arduino sketch and change communication protocol

--- Arduino_sensing/Arduino_sensing.ino
+++ Arduino_sensing/Arduino_sensing.ino
@@ -1,93 +1,67 @@
-   
-
-   
-   //****************************************************************************************
-// Illutron take on Disney style capacitive touch sensor using only passives and Arduino
-// Dzl 2012
-//****************************************************************************************
+// Implementation of
+// Touché: Enhancing Touch Interaction on Humans, Screens, Liquids, and Everyday Objects
+// by Dzl (2012), slightly cleaned up by Gohai (2025)
 
 
-//                              10n
-// PIN 9 --[10k]-+-----10mH---+--||-- OBJECT
-//               |            |
-//              3.3k          |
-//               |            V 1N4148 diode
-//              GND           |
-//                            |
-//Analog 0 ---+------+--------+
+//                               10n
+//  PIN 9 --[10k]-+-----10mH---+--||-- OBJECT
+//                |            |
+//               3.3k          |
+//                |            V 1N4148 diode
+//               GND           |
+//                             |
+// Analog 0 ---+------+--------+
 //            |      |
 //          100pf   1MOmhm
 //            |      |
 //           GND    GND
 
 
+#define N 160      // Number of frequencies
+float results[N];  // Filtered result buffer
 
-#define SET(x,y) (x |=(1<<y))				//-Bit set/clear macros
-#define CLR(x,y) (x &= (~(1<<y)))       		// |
-#define CHK(x,y) (x & (1<<y))           		// |
-#define TOG(x,y) (x^=(1<<y))            		//-+
+#define SET(x, y) (x |= (1 << y))
+#define CLR(x, y) (x &= (~(1 << y)))
 
 
+void setup() {
+  // Setup frequency generator
+  TCCR1A = 0b10000010;
+  TCCR1B = 0b00011001;
+  ICR1 = 110;
+  OCR1A = 55;
 
-#define N 160  //How many frequencies
-
-float results[N];            //-Filtered result buffer
-float freq[N];            //-Filtered result buffer
-int sizeOfArray = N;
-
- 
-   
-   
-
-void setup()
-{
-  
-  
-  TCCR1A=0b10000010;        //-Set up frequency generator
-  TCCR1B=0b00011001;        //-+
-  ICR1=110;
-  OCR1A=55;
-
-  pinMode(9,OUTPUT);        //-Signal generator pin
-  pinMode(8,OUTPUT);        //-Sync (test) pin
+  // Signal generator pin
+  pinMode(9, OUTPUT);
 
   Serial.begin(115200);
 
-  for(int i=0;i<N;i++)      //-Preset results
-    results[i]=0;         //-+
+  // Zero results array
+  for (uint8_t i = 0; i < N; i++) {
+    results[i] = 0;
+  }
 }
 
-void loop()
-{
-  unsigned int d;
+void loop() {
+  for (uint8_t d = 0; d < N; d++) {
+    uint16_t v = analogRead(0);  // Read response signal
+    CLR(TCCR1B, 0);              // Stop generator
+    TCNT1 = 0;                   // Update frequency
+    ICR1 = d;                    //
+    OCR1A = d / 2;               //
+    SET(TCCR1B, 0);              // Restart generator
 
-  int counter = 0;
-  for(unsigned int d=0;d<N;d++)
-  {
-    int v=analogRead(0);    //-Read response signal
-    CLR(TCCR1B,0);          //-Stop generator
-    TCNT1=0;                //-Reload new frequency
-    ICR1=d;                 // |
-    OCR1A=d/2;              //-+
-    SET(TCCR1B,0);          //-Restart generator
-
-    results[d]=results[d]*0.5+(float)(v)*0.5; //Filter results
-   
-    freq[d] = d;
-
- //   plot(v,0);              //-Display
- //   plot(results[d],1);
-  // delayMicroseconds(1);
+    results[d] = results[d] * 0.5f + (float)(v)*0.5f;  // Filter results
   }
 
-
-PlottArray(1,freq,results); 
- 
-
-  TOG(PORTB,0);            //-Toggle pin 8 after each sweep (good for scope)
+  for (uint8_t i = 0; i < N; i++) {
+    uint16_t out = (uint16_t)results[i];
+    uint8_t high = out >> 7;  // high 3 bits
+    if (i == 0) {
+      high |= (1 << 7);  // marker for beginning of frame
+    }
+    uint8_t low = out & 0x7f;  // low 7 bits
+    Serial.write(high);
+    Serial.write(low);
+  }
 }
-   
-
-   
-    
- 
